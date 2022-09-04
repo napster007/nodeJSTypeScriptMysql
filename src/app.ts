b236966d16802b7ef6f7d8deeba8e794e69038e0
Jayson Olaugera <jayson.olaguera@mobilemoney.ph>
2022-09-04 19:12:32 +0800

    NEw configs

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, {Application} from 'express';
+import morgan from 'morgan';
+
+
+export class App{
+    private app:Application;
+    constructor(private port?: number | string){
+        this.app = express();
+        this.settings();
+        this.moddlewares();
+    }
+
+    settings(){
+        this.app.set('port', this.port|| process.env.PORT || 3000 );
+    }
+
+    moddlewares(){
+        this.app.use(morgan('dev'));
+    }
+    async listen() {
+       await this.app.listen(this.app.get('port'));
+       console.log('Server on port', this.app.get('port'));
+    }
+}
